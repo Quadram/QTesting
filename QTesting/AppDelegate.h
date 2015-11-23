37abdb0021ed9f3e7cc433b7c4d525b650bc4124
Quadram Mobi <quadram.mobi@gmail.com>
2015-11-23 19:11:44 +0100

    Add Project

---
+++ QTesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QTesting
+//
+//  Created by Quadram Mobi on 23/11/15.
+//  Copyright © 2015 Quadram Mobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QTesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QTesting
+//
+//  Created by Quadram Mobi on 23/11/15.
+//  Copyright © 2015 Quadram Mobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

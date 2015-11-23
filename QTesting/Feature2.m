730e1e5fe0a9179fac7ad9c0b8b27cee2950d794
Quadram Mobi <quadram.mobi@gmail.com>
2015-11-23 19:23:29 +0100

    Add NSObject

---
+++ QTesting/Feature2.m
@@ -0,0 +1,13 @@
+//
+//  Feature2.m
+//  QTesting
+//
+//  Created by Quadram Mobi on 23/11/15.
+//  Copyright © 2015 Quadram Mobi. All rights reserved.
+//
+
+#import "Feature2.h"
+
+@implementation Feature2
+
+@end

b4993599bad17f491e5bf3174b6095b21aba0b53
Quadram Mobi <quadram.mobi@gmail.com>
2015-11-23 19:41:54 +0100

    Bug fixed

--- QTesting/AppDelegate.m
+++ QTesting/AppDelegate.m
@@ -47,6 +47,8 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    NSLog(@"VOY A DESCARTAR UN CAMBIO, ES DECIR UN COMMIT, y solucionado!!!");
 }
 
 @end
--- QTesting/Feature2.h
+++ QTesting/Feature2.h
@@ -10,4 +10,7 @@
 
 @interface Feature2 : NSObject
 
+@property (nonatomic, strong) NSString *name;
+
+
 @end

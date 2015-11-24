0b3285966d591b598c9178932fdb6485e9414d36
Jesús Sánchez Sánchez <er.patxi@gmail.com>
2015-11-24 17:49:30 +0100

    Add body to ViewController

--- QTesting/MainViewController.m
+++ QTesting/MainViewController.m
@@ -14,24 +14,48 @@
 
 @implementation MainViewController
 
+//-------------------------------------------------------------------------------------------------------------
+#pragma mark -
+#pragma mark - UIView LifeCycle
+#pragma mark -
+//-------------------------------------------------------------------------------------------------------------
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    //------------------------
+    
+    [self drawElements];
 }
+
+//-------------------------------------------------------------------------------------------------------------
+#pragma mark -
+#pragma mark - Help Methods
+#pragma mark -
+//-------------------------------------------------------------------------------------------------------------
+
+- (void)drawElements {
+    
+}
+
+//-------------------------------------------------------------------------------------------------------------
+#pragma mark -
+#pragma mark - Action Methods
+#pragma mark -
+//-------------------------------------------------------------------------------------------------------------
+
+
+
+//-------------------------------------------------------------------------------------------------------------
+#pragma mark -
+#pragma mark - MemoryWarning
+#pragma mark -
+//-------------------------------------------------------------------------------------------------------------
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end

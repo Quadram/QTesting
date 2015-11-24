62c9dfe3d609aa157bf75f5e659f9bc6f33a8647
Quadram Mobi <quadram.mobi@gmail.com>
2015-11-24 19:16:08 +0100

    RAMA 4

--- QTesting/ViewController.m
+++ QTesting/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"RAMAAAA 4!");
 }
 
 - (void)didReceiveMemoryWarning {

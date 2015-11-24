ca4331ee721faa3396e8636bb2c0075aa52f029e
Jesús Sánchez Sánchez <er.patxi@gmail.com>
2015-11-24 17:37:43 +0100

    Add MainViewController

---
+++ QTesting/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  QTesting
+//
+//  Created by Jesús (Patxi) Sánchez Sánchez on 24/11/15.
+//  Copyright © 2015 Quadram Mobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end

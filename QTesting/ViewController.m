89e786ef2540dff2a87f81230a8f41ef601f2adc
Quadram Mobi <quadram.mobi@gmail.com>
2015-11-26 11:12:44 +0100

    Add feature3

--- QTesting/ViewController.m
+++ QTesting/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSLog(@"ESTOY TOCANDO FEATURE 3!!!!!!! PARA LUEGO ACTUALIZAR FEATURE_LOGIN CON LOS CAMBIOS DE ESTA RAMA");
 }
 
 - (void)didReceiveMemoryWarning {
